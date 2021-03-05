82d595590b3f07eaea3fd3e838aeed9ffb871512
Max Ackerman <max@violetoffice.com>
2021-03-05 12:59:16 -0500

    test image resize

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,18 @@
 <?php snippet('header') ?>
-  <h1><?php echo $page->title()->html() ?></h1>
+  <h1><?= $page->title()->html() ?></h1>
+
+  <ul>
+  <?php foreach($site->pages() as $p): ?>
+    <li>
+      <a href="<?= $p->url() ?>"><?= $p->title() ?></a>
+    </li>
+  <?php endforeach; ?>
+  </ul>
+
+  <?php foreach($page->images() as $image): ?>
+    <?=  $image->resize(300); ?>
+  <?php endforeach; ?>
+
 <?php snippet('footer') ?>
 
 
